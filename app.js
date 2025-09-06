4ae765805867eaabf7216c51dc25342304cf9d69
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-09-06 19:50:13 +0300

    upload on vercel

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const appointmentRouter = require("./routes/appointment.route.js");
 app.use(express.json());
 app.use(cookieParser());
 const path = require("path");
+// cors
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("/", (req, res) => {
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 
 connectDB();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Running On Port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server Running On Port ${process.env.PORT}`);
+// });
 
 module.exports = app;
